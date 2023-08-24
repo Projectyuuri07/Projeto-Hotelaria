e42bdac98abc6af6b740c4b73670703ce6dd325d
Projectyuuri07 <projectyuuri07@gmail.com>
2023-08-24 08:31:19 -0300

    Update class Hotel

--- Yuri/class.py
+++ Yuri/class.py
@@ -1,10 +1,37 @@
-import os
+import os 
 
 class Hotel:
     def __init__(self, nome):
         self.nome = nome
+        self.hospedes = []
         self.quartos = []
         self.reservas = []
     
+    def add_hospede(self, hospede):
+        self.hospedes.append(hospede)
+
+    def add_reserva(self, reserva):
+        self.reservas.append(reserva)
+        print("Reserva adicionada")
+
+    #definir um limite de 2 quartos para cada tipo
+    def add_quarto(self, quarto):
+        self.quartos.append(quarto)
+        print("Quarto adicionado")
+
+    def get_reservas(self):
+        return self.reservas
+        
+    def get_hospede(self):
+        return self.hospedes
+    
+    def set_hospede(self):
+        pass
+
+    def add_quarto(self, quarto):
+        self.quartos.append(quarto)
+
+    def get_quarto(self):
+        return self.quartos
 
 
