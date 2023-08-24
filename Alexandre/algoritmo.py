5d1ab494f00bea349527e48b5e08a1570655daaa
alemes7 <alemesferreira7@gmail.com>
2023-08-24 08:22:14 -0300

    upload algoritmo

--- Alexandre/algoritmo.py
+++ Alexandre/algoritmo.py
@@ -1,9 +1,23 @@
 from classe import*
+import os
+
+
+clientes = []
+quartos = []
 
 while True:
     x = 1
     try:
         print("------------------------------------------Bem vindo ao Hotel GP2------------------------------------------")
-        escolha = int(input("[1] Fazer Login\n [2] Quartos disponiveis\n [3] Sair\n Digite sua escolha:"))
+        escolha = int(input(" [1] Fazer Login\n [2] Quartos disponiveis\n [3] Sair\n Digite sua escolha:"))
         match escolha:
             case 1:
+                os.system("cls")
+                username = input("Digite o nome de usuario:")
+                senha = input("Digite a senha:")
+
+                cliente_logado = None
+                for cliente in clientes:
+                    if cliente.validar_login(username, senha):
+                        cliente_logado = cliente
+                        break
