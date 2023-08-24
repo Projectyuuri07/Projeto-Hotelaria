dc3c0a078001c6e0025c51c563574b8a386b75a8
kauex55 <kauelima2477@gmail.com>
2023-08-24 08:12:41 -0300

    update

--- Kaue/class.py
+++ Kaue/class.py
@@ -1,1 +1,10 @@
 
+class Hotel:
+    def __init__(self, nome, endereço):
+        self.nome = nome
+        self.endereço = endereço
+        self.quartos = []
+
+    def adicionar_ap(self, quarto):
+        self.quartos.append(quarto)
+        
