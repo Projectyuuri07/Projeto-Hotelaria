89b09fc2511e1d23edf6b2e039e7f9d9f7a74251
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-08-24 08:08:14 -0300

    update

--- Vinicius/class.py
+++ Vinicius/class.py
@@ -1,23 +1,31 @@
 class Hotel:
-    def __init__(self, nome, senha):
+    def __init__(self, quantidade_quartos, quartos_disponiveis):
+        self.quantidade_quartos = quantidade_quartos
+        self.quartos_disponiveis = quartos_disponiveis
+
+    def cadastro(self, nome, senha):
         self.nome = nome
         self.senha = senha
 
     def get_nome(self):
         return self.nome
     
-    def set_nome(self,novo_nome):
+    def set_nome(self, novo_nome):
         self.nome = novo_nome
 
     def get_senha(self):
         return self.senha
     
-    def set_nome(self,nova_senha):
+    def set_nome(self, nova_senha):
         self.nome = nova_senha
 
 
     def reserva(self):
         quartos = {}
-        quarto_reservado = int(input("Digite o quarto desejado: "))
-        for i in quartos:
-            quartos.append(quarto_reservado)
+        if self.quartos_disponiveis > 0:
+            quarto_reservado = int(input("Digite o quarto desejado: "))
+            for i in range (self.quartos_disponiveis):
+                quartos.append(quarto_reservado)
+                self.quartos_disponiveis -1
+        else:
+            print("Nenhum quarto disponível!")
