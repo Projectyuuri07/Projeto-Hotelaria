4242da023e5f62026bd03b3617102a57673b7d6e
alemes7 <alemesferreira7@gmail.com>
2023-08-24 08:08:11 -0300

    upload inicio algoritmo

--- Alexandre/algoritmo.py
+++ Alexandre/algoritmo.py
@@ -1,1 +1,9 @@
+from classe import*
 
+while True:
+    x = 1
+    try:
+        print("------------------------------------------Bem vindo ao Hotel GP2------------------------------------------")
+        escolha = int(input("[1] Fazer Login\n [2] Quartos disponiveis\n [3] Sair\n Digite sua escolha:"))
+        match escolha:
+            case 1:
