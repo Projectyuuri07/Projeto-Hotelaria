5fdfb42408077e3657b6ee54605df6123e6b70ee
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-08-24 07:48:41 -0300

    criação classe hotel - vinicius

--- Vinicius/class.py
+++ Vinicius/class.py
@@ -1,1 +1,23 @@
+class Hotel:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
 
+    def get_nome(self):
+        return self.nome
+    
+    def set_nome(self,novo_nome):
+        self.nome = novo_nome
+
+    def get_senha(self):
+        return self.senha
+    
+    def set_nome(self,nova_senha):
+        self.nome = nova_senha
+
+
+    def reserva(self):
+        quartos = {}
+        quarto_reservado = int(input("Digite o quarto desejado: "))
+        for i in quartos:
+            quartos.append(quarto_reservado)
