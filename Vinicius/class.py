71ae5a0c317021e4c3dee30dcaaaab9793781c8f
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-08-24 08:22:40 -0300

    criação classe quartos - vinicius

--- Vinicius/class.py
+++ Vinicius/class.py
@@ -1,7 +1,8 @@
 class Hotel:
-    def __init__(self, quantidade_quartos, quartos_disponiveis):
+    def __init__(self, quantidade_quartos, quartos_disponiveis, quartos_indisponiveis):
         self.quantidade_quartos = quantidade_quartos
         self.quartos_disponiveis = quartos_disponiveis
+        self.quartos_indisponiveis = quartos_indisponiveis
 
     def cadastro(self, nome, senha):
         self.nome = nome
@@ -27,5 +28,13 @@ class Hotel:
             for i in range (self.quartos_disponiveis):
                 quartos.append(quarto_reservado)
                 self.quartos_disponiveis -1
+                self.quartos_indisponiveis +1
         else:
             print("Nenhum quarto disponível!")
+
+class Quartos(Hotel):
+    def características(self, camas, preço, espaço):
+        self.camas = 1
+        self.preço = 100
+        self.espaço = 1
+
