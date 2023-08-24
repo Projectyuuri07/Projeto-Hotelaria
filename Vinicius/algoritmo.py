a7370b3c40c90bfdc7fcefa61f4ff0fffa01645d
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-08-24 09:43:04 -0300

    alterações menu

--- Vinicius/algoritmo.py
+++ Vinicius/algoritmo.py
@@ -16,7 +16,7 @@ while True:
         match escolha:
             case 1:
                 os.system("cls")
-                cliente = Cliente(nome=input("Digite seu nome: "), senha=int(input("Digite sua seha: ")))
+                cliente = Cliente(nome=input("Digite seu nome: "), senha=int(input("Digite sua senha: ")))
                 print("Cadastrado com sucesso!")
                 print (f"{cliente.get_nome()}")
                 print (f"{cliente.get_senha()}")
@@ -24,11 +24,20 @@ while True:
                     cliente.append(clientes)
 
             case 2:
-                nome_login = input("Qual o seu nome: ")
+                os.system("cls")
                 for cliente in clientes:
-                    print(f"{clientes}")
+                    print(cliente)
+                nome_login = input("Qual o seu nome: ")
                 if nome_login in clientes:
                     senha_login = int(input("Digite sua senha: "))
+                    if senha_login == cliente.senha():
+                        print("Login realizado com sucesso! Encaminhando para a sessão de quartos")
+                        os.system("pause")
+                        os.system("cls")
+                        escolha = 3
+                    
+                    else:
+                        print("Login falhou, senha incorreta.")
                 
             case 3:
                 os.system("cls")
